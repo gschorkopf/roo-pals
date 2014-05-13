a99f0f2ece7749630ae89a21b9a6215f77664fb9
Geoff Schorkopf <gschorkopf@gmail.com>
2014-05-13 16:15:58 -0400

    Updates dashboard#show action

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,24 +1,8 @@
 class DashboardController < ApplicationController
   def show
-    dates = ["/2014/06/15"]
-    doc = Nokogiri::HTML(open('http://lineup.bonnaroo.com/geoff'))
-
-    @artists = []
-    doc.css('.ds-attending').each do |div|
-      artist = []
-      div.css('.ds-event-title a').each do |link|
-        if link.content.length > 75
-          artist << link.content[0..74] + "..."
-        else
-          artist << link.content
-        end
-      end
-
-      div.css('.ds-time-range').each do |span|
-        artist << span.content
-      end
-
-      @artists << artist
-    end
+    @thursday_shows = Show.where(day: "Thursday")
+    @friday_shows = Show.where(day: "Friday")
+    @saturday_shows = Show.where(day: "Saturday")
+    @sunday_shows = Show.where(day: "Sunday")
   end
 end
