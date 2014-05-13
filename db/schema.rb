e73a13bfdf6a0097af40a38f5eb6e23d944755fa
Geoff Schorkopf <gschorkopf@gmail.com>
2014-05-13 17:26:26 -0400

    Splits name column into first and last

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140513203928) do
+ActiveRecord::Schema.define(version: 20140513210835) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,12 +43,13 @@ ActiveRecord::Schema.define(version: 20140513203928) do
   add_index "shows", ["location"], name: "index_shows_on_location", using: :btree
 
   create_table "users", force: true do |t|
-    t.string   "name"
+    t.string   "first_name"
     t.string   "email"
     t.string   "password_digest"
     t.string   "auth_token"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "last_name"
   end
 
 end
