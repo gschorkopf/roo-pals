46594f3b66383eecab30f6e0f3c75f1bebab7961
Geoff Schorkopf <gschorkopf@gmail.com>
2014-05-13 15:26:52 -0400

    Autostrips some attributes

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -3,4 +3,6 @@ class Schedule < ActiveRecord::Base
   validates :user_name, :url, uniqueness: true
   has_many :schedule_shows
   has_many :shows, through: :schedule_shows
+
+ auto_strip_attributes :user_name, squish: true
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -3,4 +3,6 @@ class Show < ActiveRecord::Base
   validates :artist_name, uniqueness: true
   has_many :schedule_shows
   has_many :schedules, through: :schedule_shows
+
+  auto_strip_attributes :artist_name, :location, :day, squish: true
 end
