fb457256d1aebb806dbb7a07a1a2df9a8db3a9ed
Geoff Schorkopf <gschorkopf@gmail.com>
2014-05-13 16:47:08 -0400

    Creates some generic bcrypt methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,23 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :require_login
+  helper_method :current_user, :logged_in?
+
+  def current_user
+    @current_user ||= User.find_by(auth_token: cookies[:auth_token])
+  end
+
+  def logged_in?
+    current_user.present?
+  end
+
+  private
+
+  def require_login
+    redirect_to login_path unless logged_in?
+  end
+
+  def sign_in(user)
+    cookies.permanent[:auth_token] = user.auth_token
+  end
 end
