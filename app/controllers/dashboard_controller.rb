2df957429cb416d3c3dd6417d6d796c70eeebf03
Geoff Schorkopf <gschorkopf@gmail.com>
2014-05-14 15:20:48 -0400

    Writes schedule#create action and adds form to home page

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,5 +4,7 @@ class DashboardController < ApplicationController
     @friday_shows = Show.where(day: "Friday")
     @saturday_shows = Show.where(day: "Saturday")
     @sunday_shows = Show.where(day: "Sunday")
+    @schedule = Schedule.new
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root to: 'dashboard#show'
   get '/login', to: 'sessions#new'
   get '/logout', to: 'sessions#destroy'
+
+  resources :schedules, only: [:create, :update]
   resources :sessions, only: [:create]
   resources :users, only: [:create]
 end
