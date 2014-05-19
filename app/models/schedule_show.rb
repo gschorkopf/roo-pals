70049eb9f9b2579565d99f3f750928b090cf6612
Geoff Schorkopf <gschorkopf@gmail.com>
2014-05-19 14:28:46 -0400

    Creates validation to only add one schedule show (uniqueness)

--- app/models/schedule_show.rb
+++ app/models/schedule_show.rb
@@ -1,5 +1,7 @@
 class ScheduleShow < ActiveRecord::Base
   validates :schedule, :show, presence: true
+  validates_uniqueness_of :schedule, scope: :show
+
   belongs_to :schedule
   belongs_to :show
 end
