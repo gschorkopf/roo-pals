bec08b21fe56cf4ec25225fbd65c2f49eb5e33ae
Geoff Schorkopf <gschorkopf@gmail.com>
2014-05-18 20:58:43 -0400

    Changes name of application

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_bonnaroo-friends_session'
+Rails.application.config.session_store :cookie_store, key: '_roopals_session'
