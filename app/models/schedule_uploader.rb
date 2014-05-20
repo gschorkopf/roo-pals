f222b1d6246d42645e7ed46f1fc3480ac3685251
Geoff Schorkopf <gschorkopf@gmail.com>
2014-05-20 17:36:52 -0400

    More ScheduleUploader cleanup

--- app/models/schedule_uploader.rb
+++ app/models/schedule_uploader.rb
@@ -1,5 +1,5 @@
 class ScheduleUploader
-  attr_reader :user, :profile_name, :days
+  attr_reader :user, :profile_name
 
   def self.perform(params)
     new(params).perform
@@ -8,26 +8,36 @@ class ScheduleUploader
   def initialize(params)
     @user = params[:user]
     @profile_name = params[:profile_name]
-    @days = {
-      "2014/06/12" => "Thursday",
-      "2014/06/13" => "Friday",
-      "2014/06/14" => "Saturday",
-      "2014/06/15" => "Sunday"
-    }.freeze
   end
 
   def perform
-    days.each_pair do |date, day|
-      full_path = "http://lineup.bonnaroo.com/#{profile_name}/schedule/#{date}"
+    day_full_paths.each do |full_path|
       begin
         document = HTMLDocument.from(full_path)
       rescue
-        if schedule = Schedule.find_by(user: user, profile_name: profile_name)
-          schedule.destroy
-        end
-        return false
+        destroy_schedule
+        false
       end
       BonnarooSchedule.upload(document: document, user: user, profile_name: profile_name)
     end
   end
+
+  private
+
+  def destroy_schedule
+    if schedule = Schedule.find_by(user: user, profile_name: profile_name)
+      schedule.destroy
+    end
+  end
+
+  def day_full_paths
+    DAYS.map { |day| "http://lineup.bonnaroo.com/#{profile_name}/schedule/#{day}" }
+  end
+
+  DAYS = %w(
+    2014/06/12
+    2014/06/13
+    2014/06/14
+    2014/06/15
+  ).freeze
 end
