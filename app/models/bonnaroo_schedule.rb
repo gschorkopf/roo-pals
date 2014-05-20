023ae5f93c5b6d0de2775529aacb773bb8a89a6e
Geoff Schorkopf <gschorkopf@gmail.com>
2014-05-20 17:21:22 -0400

    Refactors BonnarooSchedule#upload

--- app/models/bonnaroo_schedule.rb
+++ app/models/bonnaroo_schedule.rb
@@ -12,15 +12,21 @@ class BonnarooSchedule
   end
 
   def upload
-    document.css('.ds-attending').each do |event|
-      artist_name = event.css('.ds-event-title a').children.first.content
-      range = event.css('.ds-time-range').children.first.content
-      starting_at = range.split(" - ").first
-      show = Show.find_by(artist_name: artist_name)
-      if show
+    events.each do |event|
+      if show = Show.find_by(artist_name: artist_name(event))
         schedule = Schedule.find_or_create_by(user: user, url: url)
         ScheduleShow.create(show: show, schedule: schedule)
       end
     end
+  end
+
+  private
+
+  def events
+    document.css('.ds-attending')
+  end
+
+  def artist_name(event)
+    event.css('.ds-event-title a').children.first.content
   end
 end
