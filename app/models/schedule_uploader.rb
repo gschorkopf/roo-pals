260da7b50520cf267f6ad338056579292d02f89c
Geoff Schorkopf <gschorkopf@gmail.com>
2014-05-20 17:01:30 -0400

    Begins the ScheduleUploader cleanup

--- app/models/schedule_uploader.rb
+++ app/models/schedule_uploader.rb
@@ -1,14 +1,22 @@
 class ScheduleUploader
+  attr_reader :user, :url, :days
+
   def self.perform(params)
-    user = params[:user]
-    url = params[:url]
-    days = {
+    new(params).perform
+  end
+
+  def initialize(params)
+    @user = params[:user]
+    @url = params[:url]
+    @days = {
       "2014/06/12" => "Thursday",
       "2014/06/13" => "Friday",
       "2014/06/14" => "Saturday",
       "2014/06/15" => "Sunday"
-    }
+    }.freeze
+  end
 
+  def perform
     days.each_pair do |date, day|
       full_path = "http://lineup.bonnaroo.com/#{url}/schedule/#{date}"
 
