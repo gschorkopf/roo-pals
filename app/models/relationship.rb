6ef03659b4d45d13ad55772b6e93469fb2f29871
Geoff Schorkopf <gschorkopf@gmail.com>
2014-06-01 19:43:04 -0400

    Adds validation to not add self

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -2,4 +2,11 @@ class Relationship < ActiveRecord::Base
   belongs_to :follower, class_name: "User"
   belongs_to :followed, class_name: "User"
   validates :follower_id, :followed_id,  presence: true
+  validate :cannot_add_self
+
+  private
+
+  def cannot_add_self
+    errors.add(:follower_id, I18n.t('errors.follower_id')) if followed_id == follower_id
+  end
 end
