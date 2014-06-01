81ab954a7a090f88b79705978da2e7a22c21bb24
Geoff Schorkopf <gschorkopf@gmail.com>
2014-06-01 19:43:15 -0400

    Adds some utility methods to followers

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,6 +23,18 @@ class User < ActiveRecord::Base
     # TODO: This will be refactored once we do multiple festivals.
   end
 
+  def following?(other_user)
+    relationships.find_by(followed_id: other_user.id)
+  end
+
+  def follow!(other_user)
+    relationships.create!(followed_id: other_user.id)
+  end
+
+  def unfollow!(other_user)
+    relationships.find_by(followed_id: other_user.id).destroy
+  end
+
   private
 
   def generate_token(column)
