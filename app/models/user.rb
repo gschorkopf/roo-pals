5b101101deae5f2c4bed9674a528faa4e0128d9c
Geoff Schorkopf <gschorkopf@gmail.com>
2014-06-01 19:42:55 -0400

    Adds follower/followed_users relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,12 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :schedules
 
+  has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+  has_many :followed_users, through: :relationships, source: :followed
+
+  has_many :reverse_relationships, foreign_key: "followed_id", class_name: "Relationship", dependent: :destroy
+  has_many :followers, through: :reverse_relationships, source: :follower
+
   validates :first_name, :last_name, :email, presence: true
   validates :password, presence: true, on: :create
   validates :email, uniqueness: true
