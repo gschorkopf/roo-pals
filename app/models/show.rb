c4d48c5ed149a29955818eff180b3b645c269cb7
Geoff Schorkopf <gschorkopf@gmail.com>
2014-06-04 16:21:52 -0400

    Adds dependent destroy where need be

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,7 +1,7 @@
 class Show < ActiveRecord::Base
   validates :artist_name, :starting_at, :ending_at, :day, :location, presence: true
   validates :artist_name, uniqueness: true
-  has_many :schedule_shows
+  has_many :schedule_shows, dependent: :destroy
   has_many :schedules, through: :schedule_shows
 
   scope :by_starting_at, -> { order(:starting_at, :ending_at) }
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   has_secure_password
-  has_many :schedules
+  has_many :schedules, dependent: :destroy
 
   has_many :relationships, foreign_key: "follower_id", dependent: :destroy
   has_many :followed_users, through: :relationships, source: :followed
