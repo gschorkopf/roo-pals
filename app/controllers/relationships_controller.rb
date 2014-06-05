9b0a8c150af38a2a7ebd10d3ab6972504d600afc
Geoff Schorkopf <gschorkopf@gmail.com>
2014-06-05 17:32:52 -0400

    Writes relationships#create action

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,4 +1,18 @@
 class RelationshipsController < ApplicationController
   def create
+    first_name, last_name = relationship_params[:follower_id].split(" ")
+    user = User.find_by(first_name: first_name, last_name: last_name)
+
+    if user.present?
+      Relationship.create(followed_id: user.id, follower_id: current_user.id)
+    end
+    redirect_to root_path
+  end
+
+  private
+
+  def relationship_params
+    params.require(:relationship)
+      .permit(:follower_id)
   end
 end
